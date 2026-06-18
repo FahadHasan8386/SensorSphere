e381766f582258d0171bdc30bebf572c8c76a308
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-18 10:39:37 +0600

    Add Models

--- sensorSphere/sensorSphere/Models/SensorReading.cs
+++ sensorSphere/sensorSphere/Models/SensorReading.cs
@@ -2,5 +2,14 @@
 {
     public class SensorReading
     {
+        public int Id { get; set; }
+        public int DeviceId { get; set; }
+        public string SensorType { get; set; } = string.Empty;
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public double COLevel { get; set; }
+        public double H2SLevel { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+
     }
 }
