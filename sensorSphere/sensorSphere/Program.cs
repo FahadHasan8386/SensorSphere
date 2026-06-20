9022f2980ba55ba298c7d70056169cad41620ada
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-20 17:58:34 +0600

    Edit some File

--- sensorSphere/sensorSphere/Program.cs
+++ sensorSphere/sensorSphere/Program.cs
@@ -1,11 +1,23 @@
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using sensorSphere;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using sensorSphere.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Register services
+builder.Services.AddScoped<AuthStateService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<DeviceStateService>();
+builder.Services.AddScoped<SensorStateService>();
+builder.Services.AddScoped<AlertStateService>();
+
+builder.Services.AddAuthorizationCore(); // Required for [Authorize]
 
 await builder.Build().RunAsync();
