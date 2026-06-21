2328ce95f0b0886c7df3f87333a6f1e87c858f16
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-21 19:03:23 +0600

    Add Model

---
+++ SensorSphereLearning/SensorSphereLearning/Model/Sensor.cs
@@ -0,0 +1,12 @@
+﻿namespace SensorSphereLearning.Model
+{
+    public class Sensor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public bool IsOnline { get; set; }
+        public string Status => IsOnline ? "Online" : "Offline";
+    }
+}
