2bb18bcfc7b618ad9d53d9eb0ea0e4e8f62c3d98
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-21 19:44:13 +0600

    Add Layout Nav Link

---
+++ SensorSphereLearning/SensorSphereLearning/Services/AppState.cs
@@ -0,0 +1,6 @@
+﻿namespace SensorSphereLearning.Services
+{
+    public class AppState
+    {
+    }
+}
---
+++ SensorSphereLearning/SensorSphereLearning/Services/SensorService.cs
@@ -0,0 +1,19 @@
+﻿using SensorSphereLearning.Model;
+
+namespace SensorSphereLearning.Services
+{
+    public class SensorService
+    {
+        private readonly List<Sensor> _sensors = new()
+        {
+            new Sensor { Id = 1, Name = "DHT22",  Temperature = 24.5, Humidity = 55, IsOnline = true  },
+            new Sensor { Id = 2, Name = "MQ7",    Temperature = 26.0, Humidity = 40, IsOnline = true  },
+            new Sensor { Id = 3, Name = "MQ136",  Temperature = 23.8, Humidity = 48, IsOnline = false },
+        };
+
+
+        private int _nextId = 4;
+
+
+    }
+}
