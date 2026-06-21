9cc67f8c5ca2472d0ec020145e68d697c23c143a
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-21 20:36:12 +0600

    Add Dashboard pages

--- SensorSphereLearning/SensorSphereLearning/Services/AppState.cs
+++ SensorSphereLearning/SensorSphereLearning/Services/AppState.cs
@@ -2,5 +2,20 @@
 {
     public class AppState
     {
+        private int TotalSensors
+        {
+            get => _totalSensors;
+            private set => _totalSensors = value;
+        }
+
+        private event Action? OnChange;
+
+        public void SetTotalSensors(int total)
+        {
+            _totalSensors = total;
+            NotifyStateChange();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
     }
 }
