06cc381219d4e9c23767b698134543ec81bfbee6
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-22 08:22:42 +0600

    Fix Some Error

--- SensorSphereLearning/SensorSphereLearning/Services/AppState.cs
+++ SensorSphereLearning/SensorSphereLearning/Services/AppState.cs
@@ -2,18 +2,20 @@
 {
     public class AppState
     {
-        private int TotalSensors
+        private int _totalSensors;
+
+        public int TotalSensors
         {
             get => _totalSensors;
             private set => _totalSensors = value;
         }
 
-        private event Action? OnChange;
+        public event Action? OnChange;
 
         public void SetTotalSensors(int total)
         {
             _totalSensors = total;
-            NotifyStateChange();
+            NotifyStateChanged(); 
         }
 
         private void NotifyStateChanged() => OnChange?.Invoke();
